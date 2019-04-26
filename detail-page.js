1bf5f180cd5026196516e7130d39314330aab799
Drashti Gadhia <drashtibgadhia10@gmail.com>
2019-04-26 13:30:54 +0200

    Setup form Validation

--- detail-page.js
+++ detail-page.js
@@ -7,6 +7,7 @@ function submitComment(){
     const textArea = document.getElementById('msg');
     let msg = textArea.value;
     console.log(msg);
+   
     //create the elements using createElement()
     const comment = document.createElement('section');
     const h3 = document.createElement('h3');
@@ -21,8 +22,29 @@ function submitComment(){
     //display the elements on the page
     const commentSection = document.getElementById('comments');
     console.log(commentSection);
+    commentSection.appendChild(comment);
     //reset field 
     inputField.value = null;
     textArea.value = null;
+    if(doesNotPassAllValidations(name , msg))return null;
+    
+    
 
 }
+function doesNotPassAllValidations(name , msg){
+    if (!name || !msg ){
+        alert("Name and Msg Should not be epmty!!")
+        return true;
+    }
+    if(!name) {alert('Name is empty');}
+    if(!msg) {alert('Msg is empty');}
+    if(!name.match(/^[A-Z]/)){
+        return name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+    }
+    if(msg.length > 100){
+        alert("Your msg is not longer than 100 words...!!")
+        return true;
+    }
+    return false;
+}
+// let p = document.getElementById('p1').innerHTML = 'hii';
